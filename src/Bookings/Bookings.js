4116def580dd63fb2ae5b637c3fa56ed0f3e0d1f
Jia Man <jman22@uwo.ca>
2022-11-19 04:18:34 -0500

    first commit for bookings

--- src/Bookings/Bookings.js
+++ src/Bookings/Bookings.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import HomeMenu from '../shared/HomeMenu'
+import "./bookings.css"
+import { DataGrid } from '@mui/x-data-grid';
 
-const Bookings = () => {
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'car', headerName: 'Car', width: 90 },
+  { field: 'carID', headerName: 'License Plate', width: 200 },
+  { field: 'confirmID', headerName: 'Confirmation Number', width: 200 },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Man', firstName: 'Jia', car: "Corolla", carID: '123', confirmID: '321' },
+  { id: 2, lastName: 'Cho', firstName: 'Dan', car: "Camry", carID: '124', confirmID: '421' },
+];
+
+export default function Bookings() {
   return (
-    <div>
-      <HomeMenu/>
-      <div>Bookings</div>
+    <div className="bookingPad" style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+      <HomeMenu />
     </div>
-  )
+  );
 }
-
-export default Bookings
