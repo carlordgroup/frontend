cfeb754fdcba05443ec055f7b68588df28fa882d
Daniel Cho <danielcho887@gmail.com>
2022-11-24 21:27:38 -0500

    update profile formatting

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -25,97 +25,97 @@ function AccountTextFields() {
 
     return (
         <div className='textFields'>
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-                onSubmit={handleSubmit}
-            >   
-            <div className="accountInfo">
-                <TextField
-                    required
-                    onChange={(e) => setfName(e.target.value)}
-                    id="firstName"
-                    label="First Name"
-                    type="firstName"
-                    InputLabelProps={{
+            <div className="profileInfo">
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={handleSubmit}
+                >   
+                <div className="accountInfo">
+                    <TextField
+                        required
+                        onChange={(e) => setfName(e.target.value)}
+                        id="firstName"
+                        label="First Name"
+                        type="firstName"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <TextField
+                        required
+                        onChange={(e) => setlName(e.target.value)}
+                        id="lastName"
+                        label="Last Name"
+                        type="lastName"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <TextField
+                        required
+                        onChange={(e) => setAdr(e.target.value)}
+                        id="address"
+                        label="Address"
+                        type="address"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <TextField
+                        required
+                        onChange={(e) => setNumber(e.target.value)}
+                        id="phoneNumber"
+                        label="Phone Number"
+                        type="phoneNumber"
+                        InputLabelProps={{
                         shrink: true,
-                    }}
-                />
-                <TextField
-                    required
-                    onChange={(e) => setlName(e.target.value)}
-                    id="lastName"
-                    label="Last Name"
-                    type="lastName"
-                    InputLabelProps={{
+                        }}
+                    />
+                    <TextField
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                        id="email"
+                        label="Email"
+                        type="email"
+                        InputLabelProps={{
                         shrink: true,
-                    }}
-                />
-            </div>
-            <div>
-                <TextField
-                    required
-                    onChange={(e) => setAdr(e.target.value)}
-                    id="address"
-                    label="Address"
-                    type="address"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-                <TextField
-                    required
-                    onChange={(e) => setNumber(e.target.value)}
-                    id="phoneNumber"
-                    label="Phone Number"
-                    type="phoneNumber"
-                    InputLabelProps={{
-                    shrink: true,
-                    }}
-                />
-            </div>
-            <div>
-                <TextField
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                    id="email"
-                    label="Email"
-                    type="email"
-                    InputLabelProps={{
-                    shrink: true,
-                    }}
-                />
-            </div>
-            <Button type="submit" className="buttonPad" variant="outlined">Save</Button>
-            </Box>
-            <div className='userInfo'> 
-                <p>
-                    First Name: {fName}
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    Last Name: {lName}
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    Address: {adr}
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    Phone Number: {number}
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    Email: {email}
-                </p>
+                        }}
+                    />
+                    <div className="buttonPad">
+                        <Button type="submit" variant="outlined" size="medium">Save</Button>
+                    </div>
+                </div>
+                </Box>
+                <div className='userInfo'> 
+                    <p>
+                        First Name: {fName}
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        Last Name: {lName}
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        Address: {adr}
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        Phone Number: {number}
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        Email: {email}
+                    </p>
+                </div>
             </div>
         </div>
     );
@@ -124,10 +124,10 @@ function AccountTextFields() {
 const Account = () => {
   return (
     <div className="accountPad" >
+        <HomeMenu />
         <h1 className="profile">Profile</h1>
         <AvatarIcon />
         <AccountTextFields />
-        <HomeMenu />
     </div>
   )
 }
