9a14bac519d66a4d50c44287bcfef3bef1c30c81
lynkas <cat@catting.net>
2022-12-02 13:12:15 -0500

    Fix frontend Login page and Register page

--- src/Login/ForgotPassword.js
+++ src/Login/ForgotPassword.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import HomeMenu from '../shared/HomeMenu';
 import Grid from '@mui/material/Grid';
 import './login.css'
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import {isEmail} from "../utils/verify";
+import Typography from "@mui/material/Typography";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("")
+  const [click, setClicked] = useState(false)
   return (
     <>
     <HomeMenu/>
     <Grid container spacing={3}>
       <Grid xs={12}>
-        <p className='resetText'>
+        <div
+            className='resetText'
+        >
+          <Typography variant="h3" style={{textAlign:"center",paddingBottom:"30px", paddingTop:"30px"}}>Forget Password</Typography>
+
+          <TextField
+              required
+              id="outlined-required"
+              label="Email"
+              defaultValue=""
+              onChange={(e)=>setEmail(e.target.value)}
+              type="email"
+              className="loginSpacing resetText"
+          />
+          <div style={{paddingTop:"30px"}}>
+            <Button disabled={!isEmail(email)} onClick={() => setClicked(true)} variant="outlined">Send Email</Button>
+          </div>
+        </div>
+        {click&&<p className='resetText'>
           An email has been sent with instructions to reset your password.
-        </p>
+        </p>}
       </Grid>
     </Grid>
     </>
