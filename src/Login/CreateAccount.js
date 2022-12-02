749bac8fe3f4a462d1a2bf44a44619e6917d7b09
lynkas <cat@moki.cat>
2022-12-02 14:05:05 -0500

    CAR-50: Fixing Login and Card Page (#15)

--- src/Login/CreateAccount.js
+++ src/Login/CreateAccount.js
@@ -5,52 +5,50 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import './login.css'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import {isEmail} from "../utils/verify";
 
 const CreateAccount = () => {
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-  const [confirmPassword, setConfirmPassword] = useState()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
 
-  const [successful, setSuccessful] = useState()
-  const [create, setCreate] = useState()
 
   const [attempt, setAttempt] = useState()
+  const [error, setError] = useState("")
   const navigate = useNavigate();
 
 
-  useEffect(() => {
-    if(create && password === confirmPassword){
-      axios.post(`https://carlord.moki.cat/api/account/register`, JSON.stringify({email: email, password: password}))
+  const register=()=>{
+    if (confirmPassword!==password){
+      setError("Password does not match")
+      return
+    }
+    if (!isEmail(email)){
+      setError("Please input a valid Email")
+      return
+    }
+    axios.post(`https://carlord.moki.cat/api/account/register`, JSON.stringify({email: email, password: password}))
         .then(res => {
-          if(res.status === 201){
-            setSuccessful(true)
-          }
+          navigate('/login')
         }).catch((error) => {
-          console.log(error.response.data)
-        })
-    }
-    if(password !== confirmPassword){
-      setSuccessful(false)
-    }
-    setCreate(false)
-  }, [create, email, password, confirmPassword])
+      setError("Email is used")
+      console.log(error.response.data)
 
-  useEffect(()=>{
-    if(successful){
-      navigate('/login')
-    }
-  },[successful, navigate])
+    })
+  }
 
   return (
     <>
     <HomeMenu/>
     <Grid container spacing={3}>
       <Grid xs={12} className="loginGrid">
+        <Typography variant="h3" style={{textAlign:"center"}}>Register</Typography>
         <div className="errorText">
-          {attempt && <p>Email is in use or passwords do not match*</p>}
+          {attempt && <p>{error}</p>}
         </div>
         <Box
           component="form"
@@ -61,39 +59,46 @@ const CreateAccount = () => {
           autoComplete="off"
           className="createAccountForm"
         >
-          <div> 
+          <div>
             <TextField
-              required
-              id="outlined-required"
-              label="Email"
-              defaultValue=""
-              value={email}
-              onChange={(input)=>{setEmail(input.target.value)}}
-              className="loginSpacing"
+                required
+                id="outlined-required"
+                label="Email"
+                defaultValue=""
+                value={email}
+                type="email"
+                onChange={(input)=>{
+                  console.log(input)
+                  setEmail(input.target.value)
+                }}
+                className="loginSpacing"
             />
             <br></br>
             <TextField
-              required
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              value={password}
-              onChange={(input)=>{setPassword(input.target.value)}}
+                required
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(input)=>{setPassword(input.target.value)}}
             />
             <br></br>
             <TextField
-              required
-              id="outlined-confirm-password-input"
-              label="Re-enter password"
-              type="password"
-              autoComplete="current-password"
-              value={confirmPassword}
-              onChange={(input)=>{setConfirmPassword(input.target.value)}}
+                required
+                id="outlined-confirm-password-input"
+                label="Re-enter password"
+                type="password"
+                autoComplete="current-password"
+                value={confirmPassword}
+                onChange={(input)=>{setConfirmPassword(input.target.value)}}
             />
             <br></br>
             <div className="loginButton">
-              <Button onClick={() => {setCreate(true); setAttempt(true)}} variant="outlined">Create Account</Button>
+              <Button onClick={() => {
+                setAttempt(true)
+                register()
+              }} variant="outlined">Create Account</Button>
             </div>
           </div>
         </Box>
